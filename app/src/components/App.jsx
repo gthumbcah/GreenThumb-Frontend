2b0daabf423931737dfadde8478a84f51b512df6
Connor <connor.ross132@gmail.com>
2024-02-21 09:13:55 +1000

    Fixed functionality for routes

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -4,23 +4,21 @@ import Home from './Home.jsx'
 import Calendar from './Calendar.jsx'
 import Admin from './Admin.jsx'
 import NewJob from './NewJob.jsx'
-import { BrowserRouter, Routes } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 const App = () =>  {
   return (
 <>
-  <h1>Green Thumb Landscaping</h1>
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/calendar" element={<Calendar />}/>
-      <Route path="new job" element={<NewJob />}/>
-          </Routes>
-  </BrowserRouter>
-{/* {  <Home />
-  <Calendar />
-  <NewJob />
-  <Admin />} */}
+      <h1>Green Thumb Landscaping</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Calendar" element={<Calendar />} />
+          <Route path="/NewJob" element={<NewJob />} />
+          <Route path="/Admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
+
 
 </>
   )
