78d8bef2b3424faca3cda478e30aedc8fafdf2d8
Connor <connor.ross132@gmail.com>
2024-02-20 14:50:39 +1000

    adding routes to different pages

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,23 +1,28 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
+import Home from './Home.jsx'
+import Calendar from './Calendar.jsx'
+import Admin from './Admin.jsx'
+import NewJob from './NewJob.jsx'
+import { BrowserRouter, Routes } from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () =>  {
   return (
-    <>
-      <div>
+<>
+  <h1>Green Thumb Landscaping</h1>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/calendar" element={<Calendar />}/>
+      <Route path="new job" element={<NewJob />}/>
+          </Routes>
+  </BrowserRouter>
+{/* {  <Home />
+  <Calendar />
+  <NewJob />
+  <Admin />} */}
 
-  </div>
-      <h1 className='Title'>Green Thumb Landscaping</h1>
-      <div className="card">
-        <input type="email" name="" id="" />
-        <input type="Password" />
-        <button className='login'>Login
-        </button>
-
-      </div>
-    </>
+</>
   )
 }
 
