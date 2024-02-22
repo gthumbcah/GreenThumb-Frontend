014c5a9090d98cc2d3eee788048926b7c749c54c
Connor <connor.ross132@gmail.com>
2024-02-22 12:08:09 +1000

    fixing issue with navbar, now is showing properly and hamburger menu on smaller screens

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -3,33 +3,27 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
-  const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(true);
   const [isRotated, setIsRotated] = useState(false);
 
   const toggleMenu = () => {
-    setShowMenu(!showMenu);
     toggleLinks(); // Toggle links visibility
     setIsRotated(!isRotated); // Toggle rotation
   };
 
   const toggleLinks = () => {
-    const links = document.getElementById("myLinks");
-    if (links.style.display === "block") {
-      links.style.display = "none";
-    } else {
-      links.style.display = "block";
-    }
+    setShowMenu(!showMenu); // Toggle menu visibility
   };
 
   return (
     <div className="topnav">
       <Link to="/" className="active">Logo</Link>
       <div id="myLinks" className={showMenu ? 'show' : ''}>
-        <Link to="/Home" onClick={toggleMenu}>Home</Link>
-        <Link to="/Calendar" onClick={toggleMenu}>Calendar</Link>
-        <Link to="/NewJob" onClick={toggleMenu}>New Job</Link>
-        <Link to="/Admin" onClick={toggleMenu}>Admin</Link>
-        <Link to="/Login" onClick={toggleMenu}>Login</Link>
+        <Link to="/Home">Home</Link>
+        <Link to="/Calendar">Calendar</Link>
+        <Link to="/NewJob">New Job</Link>
+        <Link to="/Admin">Admin</Link>
+        <Link to="/Login">Login</Link>
       </div>
       <button className={`icon ${isRotated ? 'close' : ''}`} onClick={toggleMenu}>
         {showMenu ? '✕' : '☰'}
