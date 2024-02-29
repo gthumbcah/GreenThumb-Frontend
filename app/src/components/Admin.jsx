24c142f786a80e2a091c4a464ae56573b23e73f8
Connor <connor.ross132@gmail.com>
2024-02-29 15:51:12 +1000

    adding Admin styling and confirmation button styling

--- app/src/components/Admin.jsx
+++ app/src/components/Admin.jsx
@@ -1,93 +1,100 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { API_BASE_URL } from '../components/api/endpoints.js'
-import Confirm from  './Confirm.jsx'
-
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { API_BASE_URL } from '../components/api/endpoints.js';
+import Confirm from './Confirm.jsx';
+import './Admin.css'; 
 
 const Admin = () => {
-
-  const [employees, setEmployees] = useState([])
-  const [deleteOpt, setDeleteOpt] = useState(false)
-  const [confirmation, setConfirmation] = useState(false)
-  const [deleteEmployeeId, setDeleteEmployeeId] = useState(null)
+  const [employees, setEmployees] = useState([]);
+  const [deleteOpt, setDeleteOpt] = useState(false);
+  const [confirmation, setConfirmation] = useState(false);
+  const [deleteEmployeeId, setDeleteEmployeeId] = useState(null);
 
   useEffect(() => {
-  
-
-    fetch(`${API_BASE_URL}/users`,{
+    fetch(`${API_BASE_URL}/users`, {
       method: 'GET',
       headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`
-      }
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
     })
-   .then(res => res.json())
-   .then(data => {setEmployees(data)})
-  },[employees])
+      .then((res) => res.json())
+      .then((data) => {
+        setEmployees(data);
+      });
+  }, [employees]);
 
-  const handleDelete = ((id)  => {
-    setDeleteEmployeeId(id)
-    setConfirmation(true)      
-  })
+  const handleDelete = (id) => {
+    setDeleteEmployeeId(id);
+    setConfirmation(true);
+  };
 
   const handleConfirmSubmit = (id) => {
-    fetch(`${API_BASE_URL}/users/${deleteEmployeeId}`,{
+    fetch(`${API_BASE_URL}/users/${deleteEmployeeId}`, {
       method: 'DELETE',
       headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`
-      }
-    })
-    .then(res => res.json())
-    setConfirmation(false)
-  }
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    }).then((res) => res.json());
+    setConfirmation(false);
+  };
 
   const handleCancelSubmit = () => {
-    setConfirmation(false)
-  }
+    setConfirmation(false);
+  };
 
-
-  const view = () => {
-
-    if (!deleteOpt) {
-      return (
+  return (
+    <div className="admin-container">
+      {!deleteOpt ? (
         <>
-          <ul>
+          <ul className="employee-list">
             {employees.map((employee, index) => (
-              <ul key={index}>
-                <Link to={`/edit/${employee._id}`} >{employee.name}</Link>            
-              </ul>
+              <li className="employee-item" key={index}>
+                <Link to={`/edit/${employee._id}`}>{employee.name}</Link>
+              </li>
             ))}
           </ul>
-          <div>
-            <button type="Link"><Link to={`/Admin/create`}>Create Employee</Link></button>
-            <button onClick={() => setDeleteOpt(true)}>Delete Employee</button>
+          <div className="buttons-container">
+            <button className="button" type="Link">
+              <Link to={`/Admin/create`}>Create Employee</Link>
+            </button>
+            <button className="button" onClick={() => setDeleteOpt(true)}>
+              Delete Employee
+            </button>
           </div>
         </>
-    )} else {
-      return(
+      ) : (
         <>
-          <ul>
-            {employees.map((employee, index) => ( !employee.admin && (
-              <ul key={index}>
-                <button className='deleteEm'
-                  onClick={() => handleDelete(employee._id)}>{employee.name}</button>
-              </ul>)
-            ))}
+          <ul className="employee-list">
+            {employees.map(
+              (employee, index) =>
+                !employee.admin && (
+                  <li className="employee-item" key={index}>
+                    <button
+                      className="deleteEm button"
+                      onClick={() => handleDelete(employee._id)}
+                    >
+                      {employee.name}
+                    </button>
+                  </li>
+                )
+            )}
           </ul>
-          <div>
-          <button onClick={() => setDeleteOpt(false)}>Return to Employee Selector</button>
+          <div className="buttons-container">
+            <button className="button" onClick={() => setDeleteOpt(false)}>
+              Return to Employee Selector
+            </button>
           </div>
           {confirmation && (
-                <Confirm
-                message="Are you sure you want to update the account details?"
-                onConfirm={handleConfirmSubmit}
-                onCancel={handleCancelSubmit}
-                />
-          )} 
+            <Confirm
+              message="Are you sure you want to update the account details?"
+              onConfirm={handleConfirmSubmit}
+              onCancel={handleCancelSubmit}
+            />
+          )}
         </>
-      )
-    }
-  }
-  return view() 
-}
+      )}
+    </div>
+  );
+};
 
-export default Admin
+export default Admin;
