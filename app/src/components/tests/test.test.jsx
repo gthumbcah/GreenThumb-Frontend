21564981c7a13779506be16554fb8a658ae56275
Connor <connor.ross132@gmail.com>
2024-03-02 10:34:19 +1000

    Updating testing code

--- app/src/components/tests/test.test.jsx
+++ app/src/components/tests/test.test.jsx
@@ -1,5 +1,8 @@
-import { afterEach, describe, expect, it, test, vi } from 'vitest';
+// import { afterEach, describe, expect, it, test, vi } from 'vitest';
 
-test('example test', () => {
-  expect(true).toBe(true);
-});
+// test('example test', () => {
+//   expect(true).toBe(true);
+// });
+
+
+// Testing the vitest for bug fixings
